152ef0afb81ebb2b29294cf27a928ca62512fd31
David Audet <david.audet@fivesgroup.com>
2021-06-07 15:32:59 -0400

    Change to custom request only

--- custom_components/formulaone_api/sensor.py
+++ custom_components/formulaone_api/sensor.py
@@ -5,8 +5,11 @@ https://github.com/JayBlackedOut/hass-nhlapi/blob/master/README.md
 
 import logging
 from datetime import timedelta, datetime as dt
-from formula1 import F1
-import voluptuous as vol
+import random
+import requests
+
+from typing import Optional
+from dataclasses import dataclass
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (CONF_NAME, CONF_ID, CONF_SCAN_INTERVAL)
@@ -40,7 +43,48 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     scan_interval = config.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL)
     add_entities([FormulaOneSensor(name, scan_interval, hass)])
 
+@dataclass
+class ErgastResponse(object):
+    """
+    makes the request to the api
+    url: [str] request url
+    offset: [int] starting point of elements from API request
+    limit: [int] number of items to return per request
+    """
 
+    url: str
+    offset: Optional[int] = None
+    limit: Optional[int] = None
+    _json = None
+    _xml = None
+    _text = None
+
+    def make_request(self, format_):
+        self.url = f"{self.url}{format_}"
+        if self.limit and self.offset:
+            querystring = {"limit": self.limit, "offset": self.offset}
+        else:
+            querystring = None
+        return requests.get(self.url, params=querystring)
+
+    @property
+    def xml(self):
+        if self._xml is None:
+            self._xml = self.make_request(".xml")
+        return self._xml.text
+
+    @property
+    def json(self):
+        if self._json is None:
+            self._json = self.make_request(".json")
+        return self._json.json()
+
+    @property
+    def text(self):
+        if self._text is None:
+            self._text = self.make_request(".xml")
+        return self._text.text
+        
 class FormulaOneSensor(Entity):
     """Representation of a Formula One sensor."""
 
